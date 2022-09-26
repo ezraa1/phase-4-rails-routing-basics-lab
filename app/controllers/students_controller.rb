b73bb381623b7738b6a7dabea0300e5fa20fb797
ezra kipchirchir <ezrakipchirchir1034@gmail.com>
2022-09-26 17:59:07 +0300

    Done

---
+++ app/controllers/students_controller.rb
@@ -0,0 +1,10 @@
+class StudentsController < ApplicationController
+    def index
+        allData = Student.all
+        render json: allData
+      end
+      def grades 
+        data =  Student.order(grade: :desc)
+        render json: data
+     end
+end
